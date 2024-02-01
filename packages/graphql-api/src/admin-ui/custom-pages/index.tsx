289edf884264c145c82869a0310a3a7d82ab0879
Siow <siow.koon@exogee.com>
2024-02-01 14:25:57 +1100

    cleanup

--- packages/graphql-api/src/admin-ui/custom-pages/index.tsx
+++ packages/graphql-api/src/admin-ui/custom-pages/index.tsx
@@ -4,9 +4,6 @@ import { Posts } from './Posts';
 export const customPages = {
   routes: () => [
     {
-      // This is where Xero sends us back to after the OAuth flow.
-      // Its job is to read the code and store it in local storage, then
-      // redirect back to /.
       path: '/posts',
       element: <Posts />,
     },
